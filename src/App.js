ea962abd2d8b02c5f8f839fb8edf2ad0c733d862
Naveen <naveen.anandhan@bnpparibasfortis.com>
2019-03-26 19:15:52 +0100

    feat: Display a scoreboard of 10 Frames

--- src/App.js
+++ src/App.js
@@ -6,18 +6,42 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>
+            Bowling Game
+          </h1>
         </header>
+        <div className="scoreBoard">
+            <div className="frame">
+              <p> frame 1</p>
+            </div>
+            <div className="frame">
+              <p> frame 2</p>
+            </div>
+            <div className="frame">
+              <p> frame 3</p>
+            </div>
+            <div className="frame">
+              <p> frame 4</p>
+            </div>
+            <div className="frame">
+              <p> frame 5</p>
+            </div>
+            <div className="frame">
+              <p> frame 6</p>
+            </div>
+            <div className="frame">
+              <p> frame 7</p>
+            </div>
+            <div className="frame">
+              <p> frame 8</p>
+            </div>
+            <div className="frame">
+              <p> frame 9</p>
+            </div>
+            <div className="frame">
+              <p> frame 10</p>
+            </div>
+        </div>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { configure , shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() });
+
+describe("Bowling Game", () => {
+
+  it("Should display 10 frames in a table structure", () =>{
+    let app = shallow(<App />);
+
+    expect(app.find('.scoreBoard .frame').length).toBe(10);
+  }) 
+
 });
