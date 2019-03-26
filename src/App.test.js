079c60c088f06367fb4d38db1b24863e3213641c
Naveen <naveen.anandhan@bnpparibasfortis.com>
2019-03-26 19:20:37 +0100

    test: Add beforeEach to initialize the application

--- src/App.test.js
+++ src/App.test.js
@@ -7,16 +7,16 @@ import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
 describe("Bowling Game", () => {
+  let app;
+  beforeEach(() => {
+    app = shallow(<App />);
+  })
 
   it("Should display 10 frames in a table structure", () =>{
-    let app = shallow(<App />);
-
     expect(app.find('.scoreBoard .frame').length).toBe(10);
   }) 
 
   it("Should 2 colums in each frame", () =>{
-    let app = shallow(<App />);
-    
     let frameColumnsLength = app.find('.scoreBoard .frame').at(0).childAt(1).find("span").length;
 
     expect(frameColumnsLength).toBe(2);
