80be9e574599a109f3c9c004df09f718c50dadaa
Naveen <naveen.anandhan@bnpparibasfortis.com>
2019-04-05 17:54:27 +0200

    refactor: Check for Strike in a common function

--- src/components/Frame-component.jsx
+++ src/components/Frame-component.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const FrameComponent = function(props){
+    const isStrike = (roll1) => {
+        return roll1 === 10
+    }
+
     return (
         <div className="frame">
             <p> frame 1</p>
             <div className="pins">
-                <span>{props.roll1 === 10 ? 'X' : props.roll1}</span>
-                <span>{props.roll1 === 10 ? '' : props.roll2}</span>
+                <span>{isStrike(props.roll1) ? 'X' : props.roll1}</span>
+                <span>{isStrike(props.roll1) ? '' : props.roll2}</span>
             </div>
         </div>
     );
