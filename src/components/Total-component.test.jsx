e9cb47f6dc964c2009134b66e24e249bd16fe409
Naveen <naveen.anandhan@bnpparibasfortis.com>
2019-04-15 12:21:46 +0200

    test: Add the missing test file in the previous commit

--- src/components/Total-component.test.jsx
+++ src/components/Total-component.test.jsx
@@ -5,7 +5,7 @@ import TotalComponent from './Total-component';
 describe("Bowling Game Score Calculator", () => {
   let frame;
 
-  it("Should calculate the total score of 0 when all the rolls are 0", () =>{
+  it("Should calculate the total score as 0 when all the rolls are 0", () =>{
     let total = shallow(<TotalComponent scoreBoard={[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}/>);
     let score = total.find('.total span').at(1).text();
     expect(score).toBe("0");
